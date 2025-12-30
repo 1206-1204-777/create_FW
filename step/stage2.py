bde527e52393ae0beb6ab930094ced48c17ad7ff
Your Name <you@example.com>
2025-12-30 03:37:04 +0000

    ファイル内容を整理

--- step/stage2.py
+++ step/stage2.py
@@ -111,32 +111,3 @@ def exp(x):
     return f(x)
 def add(x0, x1):
     return Add()(x0, x1)
-
-
-
-
-# 動作実験
-x1 = Variable(np.array(3))
-x0 = Variable(np.array(15))
-ys = add(square(x0), square(x1))
-ys.backward()
-print(ys.data)
-print(x0.grad, x1.grad)
-
-x = Variable(np.array(3.0))
-y = add(x, x)
-y.backward()
-print(x.grad)
-
-x.creargrad()
-y = add(add(x, x), x)
-y.backward()
-print(x.grad)
-print(ys.generation)
-
-x = Variable(np.array(2.0))
-a = square(x)
-y = add(square(a), square(a))
-y.backward()
-print(y.data)
-print(x.grad)
--- test.py
+++
@@ -1,3 +0,0 @@
-import cupy as np
-x = np.arange(1, 4)
-print(x)
