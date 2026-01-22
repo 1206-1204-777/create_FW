0f04defa838d9505cb617ddd77387f93979bfb0b
Your Name <you@example.com>
2026-01-22 04:21:01 +0000

    sin/cos/tanhのグラフを生成

--- example/test_example.py
+++ example/test_example.py
@@ -11,11 +11,17 @@ from dezero.function import *
 x0 = Variable(np.array(2.0))
 
 y = tanh(x0)
+x0.name = "x"
+y.name = "y"
 print(y.data)
 y.backward(create_graph=True)
 
-for i in range(3):
+iters = 4
+for i in range(iters):
     gx = x0.grad
     x0.crearngrad()
     gx.backward(create_graph=True)
-    print(x0.grad.dtype)
+
+gx = x0.grad
+gx.name = "gx" + str(iters + 5)
+plot_dot_graph(gx,verbose=False, to_file='tanh.png')
